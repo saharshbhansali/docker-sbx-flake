f2905c009cd13d1ad1fa7a971889a43180344e55
saharshbhansali <saharsh.bhansali@gmail.com>
2026-05-18 22:52:49 +0530

    fix: use fetchzip instead of fetchTarball

--- pkgs/sbx.nix
+++ pkgs/sbx.nix
@@ -1,12 +1,12 @@
-{ stdenv, fetchTarball, lib }:
+{ stdenv, fetchzip, lib, ... }@args:
 
 stdenv.mkDerivation {
   pname = "sbx";
   version = "0.29.0";
 
-  src = fetchTarball {
+  src = fetchzip {
     url = "https://github.com/docker/sbx-releases/releases/download/v0.29.0/DockerSandboxes-linux.tar.gz";
-    sha256 = "";
+    sha256 = lib.fakeSha256;
   };
 
 }
