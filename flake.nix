8cc94ebdbbe3f2c04491e2a65920129927666e30
saharshbhansali <saharsh.bhansali@gmail.com>
2026-05-18 23:42:57 +0530

    feat: switch to nixos-unstable from 25.11

--- flake.nix
+++ flake.nix
@@ -1,25 +1,30 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-    outputs = {self, nixpkgs, ...}@inputs: let
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
       forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
-    in {
-        packages = forAllSys (
-          system: let
-            pkgs = import nixpkgs {
-              inherit system;
-              config = {};
-              overlays = [];
-            };
-            sbx = pkgs.callPackage ./pkgs/sbx.nix {
-                inherit (inputs) nixpkgs;
-              };
-            in {
-                default = sbx;
-              }
-        );
-      };
+    in
+    {
+      packages = forAllSys (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { };
+            overlays = [ ];
+          };
+          sbx = pkgs.callPackage ./pkgs/sbx.nix {
+            inherit (inputs) nixpkgs;
+          };
+        in
+        {
+          default = sbx;
+        }
+      );
+    };
 
-  }
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,10 @@
 # default.nix
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
 in
 {
   sbx = pkgs.callPackage ./sbx.nix { };
