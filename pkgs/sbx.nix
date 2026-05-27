49e90118c53ee595be3dd9791c9b8072e96f139b
saharshbhansali <saharsh.bhansali@gmail.com>
2026-05-27 18:04:54 +0530

    feat(wip): write the installPhase script

--- pkgs/sbx.nix
+++ pkgs/sbx.nix
@@ -5,9 +5,15 @@
   ...
 }@args:
 
+with import <nixpkgs> { };
+
 stdenv.mkDerivation {
   pname = "sbx";
   version = "0.29.0";
+
+  buildInputs = [
+    pkgs.e2fsprogs
+  ];
 
   src = fetchzip {
     url = "https://github.com/docker/sbx-releases/releases/download/v0.29.0/DockerSandboxes-linux.tar.gz";
@@ -16,6 +22,17 @@ stdenv.mkDerivation {
   };
 
   installPhase = ''
-
+    mkdir -p $out/bin
+    mkdir -p $out/libexec/lib
+    mkdir -p $out/share
+    mkdir -p $out/etc/apparmor.d
+    cp sbx $out/bin/sbx
+    cp libkrun.so $out/libexec/lib/libkrun.so
+    cp mkfs.erofs $out/libexec/mkfs.erofs
+    cp containerd-shim-nerdbox-* $out/libexec/
+    cp nerdbox-initrd-* $out/libexec/
+    cp nerdbox-kernel-* $out/libexec/
+    cp apparmor-profile $out/etc/apparmor.d/docker-sbx-nerdbox-shim
   '';
+
 }
